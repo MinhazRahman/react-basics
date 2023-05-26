02b1718be100faaaf50deb2385e206a90cf35b5e
Mohammed Rahman <minhaz357@gmail.com>
2023-05-26 12:36:53 -0400

    Create App component

---
+++ jsx/src/App.js
@@ -0,0 +1,15 @@
+// Create a component and component layout
+function App() {
+  // function name
+  const name = "John Doe"; // variables
+
+  return (
+    // return statement
+    <div>
+      <h1>Hello From React Component!</h1>
+      <h2>{name}</h2>
+    </div>
+  );
+}
+
+export default App;
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,26 +1,13 @@
 // Import the React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 // Get a reference to the div with ID root
 const divElement = document.getElementById("root");
 
 // Tell React to take control of that element
 const root = ReactDOM.createRoot(divElement);
-
-// Create a component and component layout
-function App() {
-  // function name
-  const name = "John Doe"; // variables
-
-  return (
-    // return statement
-    <div>
-      <h1>Hello From React Component!</h1>
-      <h2>{name}</h2>
-    </div>
-  );
-}
 
 // Show the component on the screen
 root.render(<App />); // pass React component as a jsx element
