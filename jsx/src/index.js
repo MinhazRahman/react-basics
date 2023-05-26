a52e331b6b9e38d7b05e88c6d06d6a925fc77f05
Mohammed Rahman <minhaz357@gmail.com>
2023-05-25 23:08:08 -0400

    Show basic content of a React app

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Import the React and ReactDOM libraries
+import React from "react"
+import ReactDOM  from "react-dom/client"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Get a reference to the div with ID root
+const divElement = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Tell React to take control of that element
+const root = ReactDOM.createRoot(divElement);
+
+// Create a component
+function App() { 
+    return <h1>Hello From React Component!</h1>
+}
+
+// Show the component on the screen
+root.render(<App />); // pass React component as a jsx element
