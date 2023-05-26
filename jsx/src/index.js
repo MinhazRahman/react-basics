a3a49021ced595f44800da882a8fbfec84d7101a
Mohammed Rahman <minhaz357@gmail.com>
2023-05-25 23:38:11 -0400

    React Component Layout

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,6 +1,6 @@
 // Import the React and ReactDOM libraries
-import React from "react"
-import ReactDOM  from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 // Get a reference to the div with ID root
 const divElement = document.getElementById("root");
@@ -8,9 +8,18 @@ const divElement = document.getElementById("root");
 // Tell React to take control of that element
 const root = ReactDOM.createRoot(divElement);
 
-// Create a component
-function App() { 
-    return <h1>Hello From React Component!</h1>
+// Create a component and component layout
+function App() {
+  // function name
+  const name = "John Doe"; // variables
+
+  return (
+    // return statement
+    <div>
+      <h1>Hello From React Component!</h1>
+      <h2>{name}</h2>
+    </div>
+  );
 }
 
 // Show the component on the screen
