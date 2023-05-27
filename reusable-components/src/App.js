1473d79b479a30ee102975e8318db165439c643c
Mohammed Rahman <minhaz357@gmail.com>
2023-05-26 21:10:36 -0400

    Add images and image props

--- reusable-components/src/App.js
+++ reusable-components/src/App.js
@@ -1,13 +1,16 @@
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 // create App component
 function App() {
   return (
     <div>
-      <div>Daily Digital Assistants</div>
-      <ProfileCard title="Siri" handle="@siri2023" />
-      <ProfileCard title="Cortana" handle="@cortana2023" />
-      <ProfileCard title="Google Talk" handle="@gtalk2023" />
+      <div>Daily Productivity Assistants</div>
+      <ProfileCard title="Alexa" handle="@alexa2023" image={AlexaImage} />
+      <ProfileCard title="Cortana" handle="@cortana2023" image={CortanaImage} />
+      <ProfileCard title="Siri" handle="@siri2023" image={SiriImage} />
     </div>
   );
 }
--- reusable-components/src/ProfileCard.js
+++ reusable-components/src/ProfileCard.js
@@ -1,8 +1,9 @@
 // create component
 // using destructuring syntax
-function ProfileCard({ title, handle }) {
+function ProfileCard({ title, handle, image }) {
   return (
     <div>
+      <img src={image} alt="dpa logo" />
       <h1>{title}</h1>
       <h2>{handle}</h2>
     </div>
