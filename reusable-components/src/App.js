ce1676f834d96bbeaacb2bbb064ea62830c58b0e
Mohammed Rahman <minhaz357@gmail.com>
2023-05-27 14:53:05 -0400

    Add CSS styles

--- reusable-components/src/App.js
+++ reusable-components/src/App.js
@@ -1,3 +1,4 @@
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
 import AlexaImage from "./images/alexa.png";
 import CortanaImage from "./images/cortana.png";
@@ -7,10 +8,44 @@ import SiriImage from "./images/siri.png";
 function App() {
   return (
     <div>
-      <div>Daily Productivity Assistants</div>
-      <ProfileCard title="Alexa" handle="@alexa2023" image={AlexaImage} />
-      <ProfileCard title="Cortana" handle="@cortana2023" image={CortanaImage} />
-      <ProfileCard title="Siri" handle="@siri2023" image={SiriImage} />
+      <section class="hero is-info">
+        <div class="hero-body">
+          <p class="title">Daily Productivity Assistants</p>
+        </div>
+      </section>
+      <div className="container is-fluid">
+        <section className="section is-small">
+          <div className="columns is-centered">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa2023"
+                image={AlexaImage}
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            nec iaculis mauris."
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana2023"
+                image={CortanaImage}
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            nec iaculis mauris."
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@siri2023"
+                image={SiriImage}
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            nec iaculis mauris."
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
--- reusable-components/src/ProfileCard.js
+++ reusable-components/src/ProfileCard.js
@@ -1,11 +1,22 @@
 // create component
 // using destructuring syntax
-function ProfileCard({ title, handle, image }) {
+function ProfileCard({ title, handle, image, description }) {
   return (
-    <div>
-      <img src={image} alt="dpa logo" />
-      <h1>{title}</h1>
-      <h2>{handle}</h2>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="dpa logo" />
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media-content">
+          <p class="title is-4">{title}</p>
+          <p class="subtitle is-6">{handle}</p>
+        </div>
+        <div class="content">
+          <p>{description}</p>
+        </div>
+      </div>
     </div>
   );
 }
