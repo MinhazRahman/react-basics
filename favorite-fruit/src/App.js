926001ce4ace1ab7546ee2355f81df9d2923832e
Mohammed Rahman <minhaz357@gmail.com>
2023-05-27 19:58:57 -0400

    Use of useState

--- favorite-fruit/src/App.js
+++ favorite-fruit/src/App.js
@@ -1,5 +1,17 @@
+import { useState } from "react";
+
 function App() {
-  return <div>Show My favorite fruits!!</div>;
+    const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+      setCount(count + 1);
+  };
+  return (
+    <div>
+          <button onClick={handleClick}>Add Fruit</button>
+          <div>Number of Fruits: { count}</div>
+    </div>
+  );
 }
 
 export default App;
