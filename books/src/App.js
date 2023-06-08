3d0bc7450d04f676ed2ef8ca38cd7855bc4d6291
Mohammed Rahman <minhaz357@gmail.com>
2023-06-08 17:17:03 -0400

    Show the list of books

--- books/src/App.js
+++ books/src/App.js
@@ -1,11 +1,20 @@
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+import { useState } from "react";
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  // create a new book and update the books state
   const createBook = (title) => {
-    console.log(`Title: ${title}`);
+    const newBook = { id: Math.round(Math.random() * 9999), title: title };
+    const updatedBooks = [...books, newBook];
+
+    setBooks(updatedBooks);
   };
   return (
-    <div>
+    <div className="app">
+      <BookList books={books} />
       <BookCreate onCreate={createBook} />
     </div>
   );
