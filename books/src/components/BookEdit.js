865f5c886bb31524701def5cfbe24b37a590b4d1
Mohammed Rahman <minhaz357@gmail.com>
2023-06-08 21:15:00 -0400

    Edit title of the book

--- books/src/components/BookEdit.js
+++ books/src/components/BookEdit.js
@@ -1,5 +1,26 @@
-function BookEdit() {
-  return <div>Book Edit</div>;
+import { useState } from "react";
+
+function BookEdit({ book, onSubmit }) {
+  const [title, setTile] = useState(book.title);
+  const handleChange = (event) => {
+    setTile(event.target.value);
+  };
+
+  // on handle submit update the title of the book
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(book.id, title);
+  };
+
+  return (
+    <div>
+      <form className="book-edit" onSubmit={handleSubmit}>
+        <label>Update Title</label>
+        <input className="input" value={title} onChange={handleChange}></input>
+        <button className="button is-primary">Save</button>
+      </form>
+    </div>
+  );
 }
 
 export default BookEdit;
